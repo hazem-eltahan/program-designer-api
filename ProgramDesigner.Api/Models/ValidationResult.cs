78a6c159c394a6083d8ce6e27d0cfbc107c46ddc
hazem-eltahan <hazem.a.eltahan@gmail.com>
2026-08-14 21:58:45 +0300

    Added validation service pipeline.

---
+++ ProgramDesigner.Api/Models/ValidationResult.cs
@@ -0,0 +1,11 @@
+﻿namespace ProgramDesigner.Api.Models
+{
+    public record ImpossiblePrerequisite(string Code, string Description);
+    public record ReachabilityWarning(string Code,string Description);
+    public class ValidationResult
+    {
+        public bool IsValid => ImpossiblePrerequisites.Count == 0;
+        public List<ImpossiblePrerequisite> ImpossiblePrerequisites { get; init; } = [];
+        public List<ReachabilityWarning> ReachabilityWarnings { get; init; } = [];
+    }
+}
